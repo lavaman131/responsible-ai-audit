51c7e9ae4f9446b8dd8f4bcfc82fb3aa9e56121d
“Samuwl <samwu@bu.edu>
2024-04-21 18:12:38 -0400

    added a dummy load_data file and updated eval functions

--- responsible_ai_audit/evaluate/eval_data.py
+++ responsible_ai_audit/evaluate/eval_data.py
@@ -1,5 +1,19 @@
-#loading the datasets
-from datasets import load_dataset
-dataset = load_dataset("social_bias_frames", split="train")
+#loading the datasets from load_data.py
+import load_data
 
-print(dataset[0])
+#plot the annotatorGender dist
+def genderDist():
+    data = load_data.getRandDataset(10)
+
+#plot annotatorRace dist
+def raceDist():
+    data = load_data.getRandDataset(10)
+
+#plot annotatorPolitics dist
+def poliDist():
+    data = load_data.getRandDataset(10)
+
+
+
+
+
--- responsible_ai_audit/evaluate/eval_model.py
+++ responsible_ai_audit/evaluate/eval_model.py
@@ -1,17 +1,11 @@
 #OK FUCKERS LETS DO THIS SHIT
 #God i just want to die
+#OK, all of the code below is just to set up a "black box" model to code with
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer, AutoConfig
 import numpy as np
 from scipy.special import softmax
-
-#loading the datasets
-from datasets import load_dataset
-dataset = load_dataset("social_bias_frames", split="train")
-
-
-# Preprocess text (username and link placeholders)
 def preprocess(text):
     new_text = []
     for t in text.split(" "):
@@ -19,23 +13,25 @@ def preprocess(text):
         t = "http" if t.startswith("http") else t
         new_text.append(t)
     return " ".join(new_text)
-
-
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
 tokenizer = AutoTokenizer.from_pretrained(MODEL)
 config = AutoConfig.from_pretrained(MODEL)
-# PT
 model = AutoModelForSequenceClassification.from_pretrained(MODEL)
-# model.save_pretrained(MODEL)
 
-text = "Covid cases are increasing fast!"
+#ok cool, now we set up the data_set. in the actual we can import data/thingy.py and load from there
+#but for now we'll just make up some fake dataset
+import load_data
+data = load_data.getRandDataset(10)
+
+dataset_text = [""]
 text = preprocess(text)
 encoded_input = tokenizer(text, return_tensors="pt")
 
-
+#This is my "black box" output of our model
 output = model(**encoded_input)
 scores = output[0][0].detach().numpy()
 
+#These are the scores I'll be analyzing 
 scores = softmax(scores)
 
 print(scores)
