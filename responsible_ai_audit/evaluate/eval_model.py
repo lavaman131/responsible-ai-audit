64f06e0479bc2a909ddbc498959dd9efda9775dc
“Samuwl <samwu@bu.edu>
2024-04-29 01:18:27 -0400

    finished eval

--- responsible_ai_audit/evaluate/eval_model.py
+++ responsible_ai_audit/evaluate/eval_model.py
@@ -1,4 +1,6 @@
 #OK, all of the code below is just to set up a "black box" model to code with
+from typing import Union
+import torch.amp
 from transformers import AutoModelForSequenceClassification
 from transformers import TFAutoModelForSequenceClassification
 from transformers import AutoTokenizer, AutoConfig
@@ -10,7 +12,9 @@ import torch
 import seaborn as sns
 from responsible_ai_audit.data import get_data
 import sklearn
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+from pathlib import Path
+from torch import nn
 
 #sns formatting
 sns.set_context("poster")
@@ -43,16 +47,12 @@ def get_val_dataset():
     dataset = load_dataset("social_bias_frames", split = "test")[:1000]
     return dataset
 
-#models
-def get_models():
-    url_list = [f"cardiffnlp/twitter-roberta-base-sentiment-latest",f"cardiffnlp/twitter-roberta-base-sentiment-latest"]
-    models = []
-    for url in url_list:
-        tokenizer = AutoTokenizer.from_pretrained(url)
-        model = AutoModelForSequenceClassification.from_pretrained(url)
-        model.to(torch.device('cpu'))
-        models.append((model,tokenizer, url))
-    return models
+def get_model(model_name: str, num_labels: int = 3, base_dir = Path("models"), device: Union[str, torch.device] = "cpu") -> nn.Module:
+    base_model = "distilbert/distilroberta-base"
+    model = AutoModelForSequenceClassification.from_pretrained(base_model, num_labels=num_labels, device_map=device)
+    state_dict = torch.load(base_dir.joinpath(model_name+".pth"), map_location=device)
+    model.load_state_dict(state_dict)
+    return model
 
 
 def get_df_w_predictions(val_data, model, tokenizer):
@@ -66,8 +66,10 @@ def get_df_w_predictions(val_data, model, tokenizer):
     encoded_inputs = [tokenizer(processed_text, return_tensors="pt") for processed_text in processed_texts]
     print("done with preprocess and encode")
     #get model outputs
-    outputs = [model(**encoded_input) for encoded_input in encoded_inputs]
-    scores = [output[0][0].detach().numpy() for output in outputs]
+    outputs = []
+    with torch.autocast(device_type="cpu", dtype=torch.bfloat16):
+        outputs = [model(**encoded_input) for encoded_input in encoded_inputs]
+    scores = [output[0][0].detach().float().numpy() for output in outputs]
     softmax_scores = [softmax(score) for score in scores]
     #
     offensives = [softmax_score[0] for softmax_score in softmax_scores]
@@ -79,16 +81,22 @@ def get_df_w_predictions(val_data, model, tokenizer):
     return df
 
 
-def get_dfs_with_labels(models, val_dataset):
+def get_dfs_with_labels(val_dataset):
+    name_list = ["white_male_conservative", "white_male_liberal", "white_female_liberal", "black_female_moderate_liberal", "white_female_conservative", "all"]
+    url = "distilbert/distilroberta-base"
     dfs_with_labels = []
-    for model, tokenizer, model_name in models:
-        dfs_with_labels.append((get_df_w_predictions(val_dataset,model,tokenizer), model_name))
+    for name in name_list:
+        tokenizer = AutoTokenizer.from_pretrained(url)
+        model = get_model(name, base_dir=Path("/Users/samuelwu/Desktop/Senior/Spring/DS682/Final Project/responsible-ai-audit/responsible_ai_audit/evaluate/models"))
+        dfs_with_labels.append((get_df_w_predictions(val_dataset,model,tokenizer), name))
     return dfs_with_labels
 
-
 #bar plot
-def bar_plot(df):
+def bar_plot(df_with_label):
+    df = df_with_label[0]
+    label = df_with_label[1]
     fig = df['Prediction'].value_counts().plot(kind='bar')
+    plt.title("Prediction Distribution: "+label, fontsize=16)
     plt.show()
     #fig.savefig('assets/barplot.png')
 
@@ -107,21 +115,23 @@ def FPR_bar_plot(dfs_with_labels):
                        sum(offensiveYN == "0.0" and prediction == 0 for offensiveYN,prediction in zip(df["offensiveYN"], df["Prediction"])),
                        sum(offensiveYN == "1.0" and prediction == 2 for offensiveYN,prediction in zip(df["offensiveYN"], df["Prediction"]))]
     FPR_df = pd.DataFrame({"Label":label, "Value":value, "Model":model})
-    sns.set_theme(rc={'figure.figsize':(25,8.27)})
-    plt.title("TP/TN/FP/FN Plot", fontsize=16)
+    sns.set_theme(rc={'figure.figsize':(80,8.27)})
+    plt.title("TP/TN/FP/FN Distribution", fontsize=16)
     plt.xlabel("Model", fontsize=14)
-    plt.ylabel("Rate", fontsize=14)
+    plt.ylabel("# of Predictions", fontsize=14)
 
     # Display the plot
     sns.barplot(x = 'Label',y = 'Value',hue = 'Model',data = FPR_df)
     plt.show()
 
-def ROC_plot(df):
+def ROC_plot(df_with_label):
+    df = df_with_label[0]
+    label = df_with_label[1]
     offYN = [1 if elem == "1.0" else 0 for elem in df["offensiveYN"]]
     fpr, tpr, thresholds = sklearn.metrics.roc_curve(offYN, df["OffensiveY Score"])
     roc_df = pd.DataFrame({'fpr':fpr, 'tpr':tpr})
     sns.set_theme(rc={'figure.figsize':(11,8)})
-    plt.title("ROC Curve", fontsize=16)
+    plt.title("ROC Curve: "+label, fontsize=16)
     plt.xlabel("FPR", fontsize=14)
     plt.ylabel("TPR", fontsize=14)
 
@@ -131,9 +141,9 @@ def ROC_plot(df):
     
 
 
-dfs = get_dfs_with_labels(get_models(), get_val_dataset())
+dfs = get_dfs_with_labels(get_val_dataset())
 
 FPR_bar_plot(dfs)
-ROC_plot(dfs[0][0])
-bar_plot(dfs[0][0])
+ROC_plot(dfs[0])
+bar_plot(dfs[0])
 
